4bf762a96dd20387ea1d322f3e8cc5c2b5ddeab8
chinasoft.hy.zhong <2089720408@qq.com>
2023-03-06 17:05:49 +0800

    inin2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-zhyDesign`
+  // This tells ESLint to load the config from the package `eslint-config-cushily`
   extends: ["zhyDesign"],
   settings: {
     next: {
